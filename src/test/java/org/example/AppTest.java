7de5ccf9a56486607ea5d827d0805de918cd569a
RehasLiamsi <nsaher91@gmail.com>
2023-01-28 20:17:43 +0100

    Update AppTest.java

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,10 +3,28 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test
     void test() {
         assertThat(true).isTrue();
+    }
+
+    @Test
+    void addTwoNumbersReturnsSum() {
+        var result = App.add(1,1);
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    void givenOddNumberOddShouldReturnTrue() {
+        assertTrue(App.odd(3));
+    }
+
+    @Test
+    void givenEvenNumberOddShouldReturnFalse() {
+        assertFalse(App.odd(2));
     }
 }
