f85792c89228893ba98bdb33778d69df7f2f7d0e
RehasLiamsi <nsaher91@gmail.com>
2023-01-28 20:16:58 +0100

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,4 +4,16 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello There!");
     }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static boolean odd(int a) {
+        if (a % 2 == 1){
+            return true;
+        }
+        else
+            return false;
+    }
 }
