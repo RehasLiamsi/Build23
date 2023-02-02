ce49a57b5e3baa7adb4fb65a8f851590c555fd15
RehasLiamsi <nsaher91@gmail.com>
2023-02-02 12:19:58 +0100

    Test for arrays with two values

--- src/test/java/org/example/calculator/CalculatorTest.java
+++ src/test/java/org/example/calculator/CalculatorTest.java
@@ -36,9 +36,17 @@ public class CalculatorTest {
 
     @Test
     void twoArraysWithSameNumbersHaveOneCommonNumber() {
-        var array1 = new int[2];
-        var array2 = new int[2];
+        var array1 = new int[]{2};
+        var array2 = new int[]{2};
 
         assertEquals(1, Calculator.commonCount(array1,array2));
+    }
+
+    @Test
+    void twoArraysWithSameNumbersHaveTwoCommonNumber() {
+        var array1 = new int[]{2,3};
+        var array2 = new int[]{2,3};
+
+        assertEquals(2, Calculator.commonCount(array1,array2));
     }
 }
