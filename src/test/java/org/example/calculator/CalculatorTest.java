f5802395297c4b75d85846c69e15730585eb0593
RehasLiamsi <nsaher91@gmail.com>
2023-02-02 13:43:31 +0100

    Add test for handling null arguments

--- src/test/java/org/example/calculator/CalculatorTest.java
+++ src/test/java/org/example/calculator/CalculatorTest.java
@@ -49,4 +49,25 @@ public class CalculatorTest {
 
         assertEquals(2, Calculator.commonCount(array1,array2));
     }
+
+    @Test
+    void twoArraysWithDifferentLengthsHaveTwoCommonNumbers() {
+        var array1 = new int[]{1,2,3};
+        var array2 = new int[]{2,3};
+
+        assertEquals(2, Calculator.commonCount(array1, array2));
+    }
+
+    @Test
+    void twoArraysWithUnorderedNumbersLengthHaveTwoCommonNumbers() {
+        var array1 = new int[]{3,1,2};
+        var array2 = new int[]{2,3};
+
+        assertEquals(2, Calculator.commonCount(array1, array2));
+    }
+
+    @Test
+    void usingNullAsArgumentShouldReturnZeroCommonNumbers() {
+        assertEquals(0, Calculator.commonCount(null,null));
+    }
 }
