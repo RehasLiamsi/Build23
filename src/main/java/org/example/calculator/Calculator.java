434e6d41a0844badf5b9095f2b66e72a300e2813
RehasLiamsi <nsaher91@gmail.com>
2023-02-06 18:53:32 +0100

    Add UnpredictableDice class and canReturnOne test

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package org.example.calculator;
 
 import java.time.LocalTime;
+import java.util.Random;
 
 public class Calculator {
     public static int test(int i, int i1) {
@@ -8,8 +9,7 @@ public class Calculator {
     }
 
     public static int commonCount(int[] array1, int[] array2) {
-        if (array1 == null || array2 == null)
-            return 0;
+        if (array1 == null || array2 == null) return 0;
         int count = 0;
         for (int j : array1) {
             for (int k : array2) {
@@ -21,8 +21,25 @@ public class Calculator {
     }
 
     public static String greeting(String name, LocalTime currentTime) {
-        if(currentTime.isAfter(LocalTime.NOON))
+        if (currentTime.isAfter(LocalTime.NOON))
             return "Good afternoon " + name;
         return "Good morning " + name;
     }
+
+    public static class Die {
+        private Random random;
+
+        public Die(){
+            random = new Random();
+        }
+
+        public Die(Random random){
+            this.random = random;
+        }
+
+        public int roll() {
+            return random.nextInt(1,7);
+        }
+    }
+
 }
