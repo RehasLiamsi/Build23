58a6a5bd2deae0c9220d82f4f3ec45b33f02ac76
RehasLiamsi <nsaher91@gmail.com>
2023-02-09 19:58:37 +0100

    Prepare IT test for Redis backed cache

---
+++ src/main/java/org/example/RedisBackedCache.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class RedisBackedCache {
+    public RedisBackedCache(String localhost, int port) {
+
+    }
+
+    public void put(String key, String value) {
+        //add code to connect to database and add key value
+    }
+
+    public String get(String key) {
+        //add code to connect to database and read value from key value
+        return null;
+    }
+}
